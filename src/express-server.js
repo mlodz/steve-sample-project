1c748c7d74b084c446e8c8c15e40f7c4e9f53c02
smlodzianoski <smlodzianoski@salesforce.com>
2023-10-07 15:54:56 -0400

    create src dir

--- src/express-server.js
+++ src/express-server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const path = require('path');
+var express = require('express');
+var app = express();
+var path = require('path');
 
 // Render a static html file at the root path
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'routes/base.html'));
-})
+});
 // TODO: load static files from a "public" dir
 app.use('/static', express.static(path.join(__dirname, 'routes/static')));
 
 // Simple route example
-app.get('/about', (req, res) => {
-  res.send('this is an about page')
-})
+app.get('/about', function (req, res) {
+  res.send('this is an about page');
+});
 
 // Setup a route file, put multiple endpoints in that file
 app.use('/api/user', require('./routes/api/user'));
 
 // Start the server
-const port = 3000
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+var port = 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -3,9 +3,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/current', function(req, res) {
+router.get('/current', function (req, res) {
     res.json(['nobody is logged in, this is routes/api path', 1234]);
 });
-
 
 module.exports = router;
